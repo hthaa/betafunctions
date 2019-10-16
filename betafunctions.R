2be99aad00150d4fa2dd49c755a77b201e6eba2a
Haakon Thorbergsen Haakstad <thorbergsen@gmail.com>
2019-10-16 14:48:55 +0200

    Case-changes. Consistency of var <-> sd.

--- betafunctions.R
+++ betafunctions.R
@@ -1,36 +1,36 @@
 #' Compute Moments of Beta Probability Density Distributions.
 #'
 #' @description Computes Raw, Central, or Standardized moment properties of defined Standard Beta probability density distributions.
-#' @param A The Alpha shape parameter of the PDD.
-#' @param B The Beta shape parameter of the PDD.
-#' @param TYPES A character vector determining which moment-types are to be calculated. Permissible values are "raw", "central", and "standardized".
-#' @param ORDERS The number of moment-orders to be calculated for each of the moment-types.
+#' @param a The Alpha shape parameter of the PDD.
+#' @param b The Beta shape parameter of the PDD.
+#' @param types A character vector determining which moment-types are to be calculated. Permissible values are "raw", "central", and "standardized".
+#' @param orders The number of moment-orders to be calculated for each of the moment-types.
 #' @return A list of moment types, each a list of moment orders.
 #' @export
-BetaMoments <- function(A, B, TYPES = c("raw", "central", "standardized"), ORDERS = 4) {
-  BETAMOMENTS <- rep(list(rep(list(NULL), ORDERS)), length(TYPES))
+betamoments <- function(a, b, types = c("raw", "central", "standardized"), orders = 4) {
+  BETAMOMENTS <- rep(list(rep(list(NULL), orders)), length(types))
   TYPE <- 1
-  if (any(TYPES == "raw")) {
-    for (i in 1:ORDERS) {
-      BETAMOMENTS[[TYPE]][[i]] <- integrate(function(x) { dbeta(x, A, B) * x^i }, lower = 0, upper = 1)$value
+  if (any(types == "raw")) {
+    for (i in 1:orders) {
+      BETAMOMENTS[[TYPE]][[i]] <- integrate(function(x) { dbeta(x, a, b) * x^i }, lower = 0, upper = 1)$value
     }
     names(BETAMOMENTS)[TYPE] <- "raw"
     TYPE <- TYPE + 1
   }
-  if (any(TYPES == "central")) {
-    Mu <- integrate(function(x) { dbeta(x, A, B) * x }, lower = 0, upper = 1)$value
-    for (i in 1:ORDERS) {
-      BETAMOMENTS[[TYPE]][[i]] <- integrate(function(x) { dbeta(x, A, B) * (x - Mu)^i},
+  if (any(types == "central")) {
+    Mu <- integrate(function(x) { dbeta(x, a, b) * x }, lower = 0, upper = 1)$value
+    for (i in 1:orders) {
+      BETAMOMENTS[[TYPE]][[i]] <- integrate(function(x) { dbeta(x, a, b) * (x - Mu)^i},
                                             lower = 0, upper = 1)$value
     }
     names(BETAMOMENTS)[TYPE] <- "central"
     TYPE <- TYPE + 1
   }
-  if (any(TYPES == "standardized")) {
-    Mu <- integrate(function(x) { dbeta(x, A, B) * x }, lower = 0, upper = 1)$value
-    Sigma <- integrate(function(x) { dbeta(x, A, B) * (x - Mu)^2 }, lower = 0, upper = 1)$value
-    for (i in 1:ORDERS) {
-      BETAMOMENTS[[TYPE]][[i]] <- integrate(function(x) { dbeta(x, A, B) * ((x - Mu)^i / sqrt(Sigma)^i)},
+  if (any(types == "standardized")) {
+    Mu <- integrate(function(x) { dbeta(x, a, b) * x }, lower = 0, upper = 1)$value
+    Sigma <- integrate(function(x) { dbeta(x, a, b) * (x - Mu)^2 }, lower = 0, upper = 1)$value
+    for (i in 1:orders) {
+      BETAMOMENTS[[TYPE]][[i]] <- integrate(function(x) { dbeta(x, a, b) * ((x - Mu)^i / sqrt(Sigma)^i)},
                                             lower = 0, upper = 1)$value
     }
     names(BETAMOMENTS)[TYPE] <- "standardized"
@@ -41,127 +41,141 @@ BetaMoments <- function(A, B, TYPES = c("raw", "central", "standardized"), ORDER
 #' Alpha shape parameter given mean and variance of a Standard Beta PDD.
 #'
 #' @description Calculates the Alpha value required to produce a Standard Beta probability density distribution with defined mean and variance or standard deviation.
-#' @param M The mean of the target Standard Beta probability density distribution.
-#' @param VAR The variance of the target Standard Beta probability density distribution.
+#' @param mean The mean of the target Standard Beta probability density distribution.
+#' @param var The variance of the target Standard Beta probability density distribution.
+#' @param sd The standard deviation of the target Standard Beta probability density distribution.
 #' @return A numeric value representing the required value for the Alpha shape-parameter in order to produce a Standard Beta probability density distribution with the target mean and variance.
 #' @export
-AMS <- function(M, VAR) {
-  if(((M^2 - M^3) / VAR) - M <= 0) {
+AMS <- function(mean, var, sd) {
+  if ((!is.null(var) & !is.null(sd))) {
+    if (var != sd^2) {
+      warning("Nonequivalent values of VAR and SD specified. Using VAR.")
+    }
+  }
+  if (is.null(var) & !is.null(sd)) var <- sd^2
+  if(((mean^2 - mean^3) / var) - mean <= 0) {
     warning("Parameter out of bounds (Alpha <= 0).")
   }
-  return(((M^2 - M^3) / VAR) - M)
+  return(((mean^2 - mean^3) / var) - mean)
 }
 
 #' Beta shape parameter given mean and variance of a Standard Beta PDD.
 #'
 #' @description Calculates the Beta value required to produce a Standard Beta probability density distribution with defined mean and variance or standard deviation.
-#' @param M The mean of the target Standard Beta probability density distribution.
-#' @param VAR The variance of the target Standard Beta probability density distribution.
+#' @param mean The mean of the target Standard Beta probability density distribution.
+#' @param var The variance of the target Standard Beta probability density distribution.
+#' @param sd The standard deviation of the target Standard Beta probability density distribution.
 #' @return A numeric value representing the required value for the Beta shape-parameter in order to produce a Standard Beta probability density distribution with the target mean and variance.
 #' @export
-BMS <- function(M, VAR) {
-  if((M * (1 - M)^2) / VAR + M - 1 <= 0) {
+BMS <- function(mean, var, sd) {
+  if ((!is.null(var) & !is.null(sd))) {
+    if (var != sd^2) {
+      warning("Nonequivalent values of VAR and SD specified. Using VAR.")
+    }
+  }
+  if (is.null(var) & !is.null(sd)) var <- sd^2
+  if((mean * (1 - mean)^2) / var + mean - 1 <= 0) {
     warning("Parameter out of bounds (Beta <= 0).")
   }
-  return(((M * (1 - M)^2) / VAR) + M - 1)
+  return(((mean * (1 - mean)^2) / var) + mean - 1)
 }
 
 #' Probability of some specific observation under the Standard Beta PDD with specific mean and variance.
 #'
 #' @description Calculates the probability of some specific observation falling under a specified interval  ([0, x] or [x, 1]) under the Standard Beta probability density distribution with defined mean and variance or standard deviation.
 #' @param q A specific point on the x-axis of the Standard Beta probability density distribution with a defined mean and variance.
-#' @param M The mean of the target Standard Beta probability density distribution.
-#' @param VAR The variance of the target Standard Beta probability density distribution.
-#' @param SD The standard deviation of the target Standard Beta probability density distribution.
+#' @param mean The mean of the target Standard Beta probability density distribution.
+#' @param var The variance of the target Standard Beta probability density distribution.
+#' @param sd The standard deviation of the target Standard Beta probability density distribution.
 #' @param lt Whether the density that should be considered is between the lower-end (i.e., [0 -> x]) or the higher-end of the distribution (i.e., [x -> 1]).
 #' @return A value representing the probability of a random draw from the Standard Beta probability density distribution with a defined mean and variance being from one of two defined intervals (i.e., [0 -> x] or [x -> 1]).
 #' @export
-pBetaMS <- function(q, M, VAR = NULL, SD = NULL, LT = TRUE) {
-  if ((!is.null(VAR) & !is.null(SD))) {
-    if (VAR != SD^2) {
+pBetaMS <- function(q, mean, var = NULL, sd = NULL, lt = TRUE) {
+  if ((!is.null(var) & !is.null(sd))) {
+    if (var != sd^2) {
       warning("Nonequivalent values of VAR and SD specified. Using VAR.")
     }
   }
-  if (is.null(VAR) & !is.null(SD)) VAR <- SD^2
-  pbeta(q, ((M^2 - M^3) / VAR) - M, (M * (1 - M)^2) / VAR + M - 1, lower.tail = LT)
+  if (is.null(var) & !is.null(sd)) var <- sd^2
+  pbeta(q, ((mean^2 - mean^3) / var) - mean, (mean * (1 - mean)^2) / var + mean - 1, lower.tail = lt)
 }
 
 #' Density under a specific point of the Standard Beta PDD with specific mean and variance or standard deviation.
 #'
 #' @description Calculates the density under specific points of the Standard Beta probability density distribution with defined mean and variance or standard deviation.
 #' @param x A specific point on the x-axis of the Standard Beta PDD.
-#' @param M The mean of the target Standard Beta probability density distribution.
-#' @param VAR The variance of the target Standard Beta probability density distribution.
-#' @param SD The standard deviation of the target Standard Beta probability density distribution.
+#' @param mean The mean of the target Standard Beta probability density distribution.
+#' @param var The variance of the target Standard Beta probability density distribution.
+#' @param sd The standard deviation of the target Standard Beta probability density distribution.
 #' @return A numeric value representing the required value for the Beta Shape-parameter in order to produce a Standard Beta probability density distribution with the target mean and variance.
 #' @export
-dBetaMS <- function(x, M, VAR = NULL, SD = NULL) {
-  if ((!is.null(VAR) & !is.null(SD))) {
-    if (VAR != SD^2) {
+dBetaMS <- function(x, mean, var = NULL, sd = NULL) {
+  if ((!is.null(var) & !is.null(sd))) {
+    if (var != sd^2) {
       warning("Nonequivalent values of VAR and SD specified. Using VAR.")
     }
   }
-  if (is.null(VAR) & !is.null(SD)) VAR <- SD^2
-  dbeta(x, ((M^2 - M^3) / VAR) - M, (M * (1 - M)^2) / VAR + M - 1)
+  if (is.null(var) & !is.null(sd)) var <- sd^2
+  dbeta(x, ((mean^2 - mean^3) / var) - mean, (mean * (1 - mean)^2) / var + mean - 1)
 }
 
 #' Quantile containing specific proportion of the distribution, given a specific probability of the Standard Beta PDD with specific mean and variance or standard deviation.
 #'
 #' @description Calculates the quantile corresponding to a specific probability of some observation falling within the [0, x] (LT = TRUE) or [x, 1] (LT = FALSE) interval under the Standard Beta probability density distribution with defined mean and variance or standard deviation.
 #' @param p A value of probability marking the point of the Y-axis to correspond to the X-axis.
-#' @param M The mean of the target Standard Beta probability density distribution.
-#' @param VAR The variance of the target Standard Beta probability density distribution.
-#' @param SD The standard deviation of the target Standard Beta probability density distribution.
-#' @param LT Logical. Specifies which end of the tail for which to calculate quantile. Default is TRUE (meaning, find q for lower tail.)
+#' @param mean The mean of the target Standard Beta probability density distribution.
+#' @param var The variance of the target Standard Beta probability density distribution.
+#' @param sd The standard deviation of the target Standard Beta probability density distribution.
+#' @param lt Logical. Specifies which end of the tail for which to calculate quantile. Default is TRUE (meaning, find q for lower tail.)
 #' @return A numeric value representing the quantile for which the specified proportion of observations fall within.
 #' @export
-qBetaMS <- function(p, M, VAR = NULL, SD = NULL, LT = TRUE) {
-  if ((!is.null(VAR) & !is.null(SD))) {
-    if (VAR != SD^2) {
+qBetaMS <- function(p, mean, var = NULL, sd = NULL, lt = TRUE) {
+  if ((!is.null(var) & !is.null(sd))) {
+    if (var != sd^2) {
       warning("Nonequivalent values of VAR and SD specified. Using VAR.")
     }
   }
-  if (is.null(VAR) & !is.null(SD)) VAR <- SD^2
-  qbeta(p, ((M^2 - M^3) / VAR) - M, (M * (1 - M)^2) / VAR + M - 1, lower.tail = LT)
+  if (is.null(var) & !is.null(sd)) var <- sd^2
+  qbeta(p, ((mean^2 - mean^3) / var) - mean, (mean * (1 - mean)^2) / var + mean - 1, lower.tail = lt)
 }
 
 #' Random draw from the Standard Beta PDD with specific mean and variance.
 #'
 #' @description Draws random samples of observations from the Standard Beta probability density distribution with defined mean and variance.
 #' @param n Number of observations to be drawn from under the Standard Beta PDD.
-#' @param M The mean of the target Standard Beta probability density distribution.
-#' @param VAR The variance of the target Standard Beta probability density distribution.
-#' @param SD The standard deviation of the target Standard probability density distribution.
+#' @param mean The mean of the target Standard Beta probability density distribution.
+#' @param var The variance of the target Standard Beta probability density distribution.
+#' @param sd The standard deviation of the target Standard probability density distribution.
 #' @return A vector of length \code{n}, each value representing a random draw from the Standard Beta probability density distribution with defined mean and variance.
 #' @export
-rBetaMS <- function(n, M, VAR = NULL, SD = NULL) {
-  if ((!is.null(VAR) & !is.null(SD))) {
-    if (VAR != SD^2) {
+rBetaMS <- function(n, mean, var = NULL, sd = NULL) {
+  if ((!is.null(var) & !is.null(sd))) {
+    if (var != sd^2) {
       warning("Nonequivalent values of VAR and SD specified. Using VAR.")
     }
   }
-  if (is.null(VAR) & !is.null(SD)) VAR <- SD^2
-  rbeta(n, ((M^2 - M^3) / VAR) - M, (M * (1 - M)^2) / VAR + M - 1)
+  if (is.null(var) & !is.null(sd)) var <- sd^2
+  rbeta(n, ((mean^2 - mean^3) / var) - mean, (mean * (1 - mean)^2) / var + mean - 1)
 }
 
 #' Coordinate generation for marking an area under the curve for the Standard Beta probability density distribution.
 #'
 #' @description Plotting tool, producing a two-column matrix with values of \code{y} corresponding to locations on \code{x}. Useful for shading areas under the curve when tracing the line for the Standard Beta probability density function.
-#' @param FROM The point of the x-axis from where to start producing y-density values.
-#' @param TO The point of the x-axis to where y-density values are to be produced.
-#' @param BY The resolution (or specing) at which to produce y-density values.
-#' @param Alpha The Alpha shape-parameter value for the Standard Beta probability density distribution.
-#' @param Beta The Beta shape-parameter fort he Standard Beta probability density distribution.
+#' @param from The point of the x-axis from where to start producing y-density values.
+#' @param to The point of the x-axis to where y-density values are to be produced.
+#' @param by The resolution (or specing) at which to produce y-density values.
+#' @param alpha The Alpha shape-parameter value for the Standard Beta probability density distribution.
+#' @param beta The Beta shape-parameter fort he Standard Beta probability density distribution.
 #' @return A two-column matrix with density-values of y to plot against corresponding location values of x.
 #' @export
-Beta.gfx.poly.pdf <- function(FROM, TO, BY, Alpha, Beta) {
-  x <- c(FROM, seq(FROM, TO, BY), TO)
+Beta.gfx.poly.pdf <- function(from, to, by, alpha, beta) {
+  x <- c(from, seq(from, to, by), to)
   for (i in 1:length(x)) {
     if (i == 1) y <- vector(length = length(x))
     if (i == 1 | i == length(x)) {
       y[i] <- 0
     } else {
-      y[i] <- dbeta(x[i], Alpha, Beta)
+      y[i] <- dbeta(x[i], alpha, beta)
     }
   }
   return(cbind(x, y))
@@ -170,21 +184,21 @@ Beta.gfx.poly.pdf <- function(FROM, TO, BY, Alpha, Beta) {
 #' Coordinate generation for marking an area under the curve for the Standard Beta cumulative probability density distribution.
 #'
 #' @description Plotting tool, producing a two-column matrix with values of \code{y} corresponding to locations on \code{x}. Useful for shading areas under the curve when tracing the line for the Standard Beta probability density function.
-#' @param FROM The point of the x-axis from where to start producing y-density values.
-#' @param TO The point of the x-axis to where y-density values are to be produced.
-#' @param BY The resolution (or specing) at which to produce y-density values.
-#' @param Alpha The Alpha shape-parameter value for the Standard Beta probability density distribution.
-#' @param Beta The Beta shape-parameter fort he Standard Beta probability density distribution.
+#' @param from The point of the x-axis from where to start producing y-density values.
+#' @param to The point of the x-axis to where y-density values are to be produced.
+#' @param by The resolution (or specing) at which to produce y-density values.
+#' @param alpha The Alpha shape-parameter value for the Standard Beta probability density distribution.
+#' @param beta The Beta shape-parameter fort he Standard Beta probability density distribution.
 #' @return A two-column matrix with density-values of y to plot against corresponding location values of x.
 #' @export
-Beta.gfx.poly.cdf <- function(FROM, TO, BY, Alpha, Beta) {
-  x <- c(FROM, seq(FROM, TO, BY), TO)
+Beta.gfx.poly.cdf <- function(from, to, by, alpha, beta) {
+  x <- c(from, seq(from, to, by), to)
   for (i in 1:length(x)) {
     if (i == 1) y <- vector(length = length(x))
     if (i == 1 | i == length(x)) {
       y[i] <- 0
     } else {
-      y[i] <- pbeta(x[i], Alpha, Beta)
+      y[i] <- pbeta(x[i], alpha, beta)
     }
   }
   return(cbind(x, y))
@@ -213,17 +227,17 @@ MLA <- function(a, b, x = NULL, N = NULL) {
 #' @param a Observed alpha value for fitted Standard Beta PDD.
 #' @param b Observed beta value for fitted Standard Beta PDD.
 #' @param x Observed proportion-correct outcome.
-#' @param N Test-length.
+#' @param n Test-length.
 #' @return The Beta shape-parameter value for the Standard Beta probability density distribution where the observed mean is the expected mode.
 #' @export
-MLB <- function(a, b, x = NULL, N = NULL) {
-  if (is.null(x) | is.null(N)) {
+MLB <- function(a, b, x = NULL, n = NULL) {
+  if (is.null(x) | is.null(n)) {
     n <- a + b
     A <- (a*(n - 2) + n) / n
     B <- n - A
     B
   } else {
-    N - (x*(N - 2) + 1)
+    n - (x*(n - 2) + 1)
   }
 }
 
@@ -236,13 +250,13 @@ MLB <- function(a, b, x = NULL, N = NULL) {
 #' @param N Test-length.
 #' @return The expected mean of the Standard Beta probability density distribution, for which the observed mean is the most likely value.
 #' @export
-MLM <- function(a, b, x = NULL, N = NULL) {
+MLM <- function(a, b, x = NULL, n = NULL) {
   if (is.null(x) | is.null(N)) {
     n <- a + b
     A <- (a*(n - 2) + n) / n
     B <- (n - A)
     A / (A + B)
   } else {
-    (x*(N - 2) + 1) / N
+    (x*(n - 2) + 1) / n
   }
 }
